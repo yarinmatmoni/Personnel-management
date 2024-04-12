449d25a3b9774a30982e4ef7fe2cae1cb085a383
yarinmatmoni <yarinmatmoni@gmail.com>
2024-04-12 13:39:51 +0300

    Header component - responsive style

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,34 @@
 import { useState } from "react";
 import { NavLink } from "react-router-dom";
+import { Hamburger } from "./index";
+import homeIcon from "../assets/home.svg";
+import contactsIcon from "../assets/contacts.svg";
 
 export const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
 
+  const onMenuClick = () => {
+    setIsOpen((prevData) => !prevData);
+  };
+
   return (
     <nav className="header">
-      <div onClick={() => setIsOpen((prevData) => !prevData)}>X</div>
+      <Hamburger onMenuClick={() => onMenuClick()} isOpen={isOpen} />
       <div className="nav-links" data-is-open={isOpen}>
-        <NavLink to="/">
-          <div className="link">Home</div>
-        </NavLink>
-        <NavLink to="/people">
-          <div className="link">People</div>
-        </NavLink>
+        <div className="links-container">
+          <NavLink to="/" onClick={() => onMenuClick()}>
+            <div className="link">
+              <img src={homeIcon} alt="home" />
+              <div className="link-name">Home</div>
+            </div>
+          </NavLink>
+          <NavLink to="/people" onClick={() => onMenuClick()}>
+            <div className="link">
+              <img src={contactsIcon} alt="contacts" />
+              <div className="link-name">People</div>
+            </div>
+          </NavLink>
+        </div>
       </div>
     </nav>
   );
